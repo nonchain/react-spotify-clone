7f5150ca5815557d8c7c7aaf47baf087aaf6877d
progali007 <code.progali@gmail.com>
2022-10-03 18:04:28 +0330

    Designed Error

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Error = () => (
-  <div>Error</div>
+  <div className='w-full h-full flex flex-col justify-center items-center'>
+    <h2 className='text-xl text-center text-white font-bold'>{'Something went wrong. Please try again'}</h2>
+  </div>
 );
 
 export default Error;
