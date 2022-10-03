44e5117a6abad24ecb873d04c91855e0daf6ce1c
progali007 <code.progali@gmail.com>
2022-10-03 21:07:49 +0330

    Designed Component

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,5 +1,38 @@
-const SongCard = ({song}) => (
-  <div className="text-white">{song}</div>
-);
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
+import { Link } from 'react-router-dom';
+import PlayPause from './PlayPause';
 
-export default SongCard;
+import { favorite } from '../assets'
+
+const SongCard = ({ song, index }) => {
+  const activeSong = 'Test';
+
+  return (
+    <div className='w-full grid grid-cols-5 backdrop-blur-sm cursor-pointer justify-between items-center opacity-80 animate-slideup'>
+      {/*===== IMAGE =====*/}
+      <div className='relative w-14 h-14 overflow-hidden group'>
+          <div className={`absolute items-center justify-center bg-black bg-opacity-50 group-hover:flex group-hover:inset-0 ${activeSong?.title === song?.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
+            <PlayPause />
+          </div>
+          <img src={song.images?.coverart} alt="song_img" className='w-full h-full rounded-lg  overflow-hidden object-cover' />
+        </div>
+        {/*===== TITLE & ARTIST =====*/}
+        <div className='ml-2 col-span-3 truncate flex flex-col gap-1'>
+          <Link to={`/songs/${song?.key}`}>
+            <h2 className='text-base text-white font-semibold truncate'>{song.title}</h2>
+          </Link>
+          <Link to={song?.artists ? `/artist/${song?.artists[0]?.adamid}` : '/top-artists'}>
+            <p className='text-xs text-gray-300 font-medium truncate'>{song.subtitle}</p>
+          </Link>
+        </div>
+      
+      <div className='flex items-center justify-end'>
+        <img src={favorite} alt="favorite_icon" className='text-green-500'/>
+      </div>
+    </div>
+  )
+}
+
+export default SongCard
