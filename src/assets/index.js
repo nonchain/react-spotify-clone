28a675139aeb11b64aeaded9aa3774885d9b4b7c
progali007 <code.progali@gmail.com>
2022-10-04 15:20:23 +0330

    Add icon

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,13 @@
 import loader from './loader.svg';
 import logo from './logo.svg';
 import favorite from './favorite.svg';
+import play from './favorite.svg';
+import pause from './favorite.svg';
 
 export {
   logo,
   loader,
   favorite,
+  pause,
+  play,
 };
