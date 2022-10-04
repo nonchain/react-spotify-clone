06b04fbb2f28146d1a2266f50c5328a9b18d9355
progali007 <code.progali@gmail.com>
2022-10-04 15:30:55 +0330

    Fix bug

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,8 @@
 import loader from './loader.svg';
 import logo from './logo.svg';
 import favorite from './favorite.svg';
-import play from './favorite.svg';
-import pause from './favorite.svg';
+import play from './play_circle.svg';
+import pause from './pause_circle.svg';
 
 export {
   logo,
