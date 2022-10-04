b2fad193342f58d752bf4765c9c36d559c07a7bf
progali007 <code.progali@gmail.com>
2022-10-04 16:01:46 +0330

    Designed and add functionality

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,11 @@
-const PlayPause = () => (
-  <div>Loader</div>
+import React from 'react';
+import { play, pause } from '../assets';
+
+const PlayPause = ({ song, isPlaying, activeSong, handelPause, handelPlay }) => (
+  isPlaying && activeSong.title === song?.title ?
+    (<img src={pause} alt='pause' onClick={handelPause}/>)
+    :
+    (<img src={play} alt='play' onClick={handelPlay}/>)
 );
 
-export default PlayPause;
+export default PlayPause
