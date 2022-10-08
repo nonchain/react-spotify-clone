a261d7dd9dc53d76f836678844b1e2f0d4d1d7e6
progali007 <code.progali@gmail.com>
2022-10-09 01:23:14 +0330

    Add className attribute

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { play, pause } from '../assets';
 
-const PlayPause = ({ song, isPlaying, activeSong, handelPause, handelPlay }) => (
+const PlayPause = ({ song, isPlaying, activeSong, handelPause, handelPlay, className }) => (
   isPlaying && activeSong.title === song?.title ?
-    (<img src={pause} alt='pause' onClick={handelPause}/>)
+    (<img src={pause} alt='pause' onClick={handelPause} className={className}/>)
     :
-    (<img src={play} alt='play' onClick={handelPlay}/>)
+    (<img src={play} alt='play' onClick={handelPlay} className={className}/>)
 );
 
 export default PlayPause
