be0ec430e77f1432eeee76c4de523d168838aea9
progali007 <code.progali@gmail.com>
2022-10-12 22:26:13 +0330

    Designed component

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,5 +1,15 @@
-const ArtistCard = () => {
-  <div>ArtistCard</div>;
-};
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default ArtistCard;
+const ArtistCard = ({track}) => {
+  const navigate = useNavigate()
+
+  return (
+    <div className='w-full flex flex-col gap-2 rounded-lg overflow-hidden lg:gap-3' onClick={()=> navigate(`/artist/${track?.artists[0].adamid}`)}>
+      <img src={track?.images?.background} alt="artist" className='w-full object-cover rounded-lg overflow-hidden cursor-pointer duration-300 hover:scale-105'/>
+      <p className='text-sm text-white font-semibold'>{track?.subtitle}</p>
+    </div>
+  )
+}
+
+export default ArtistCard
