c14b49c0767ce2c51b86083398b6f0caa701f775
progali007 <code.progali@gmail.com>
2022-10-12 13:00:21 +0330

    Designed page

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,3 +1,38 @@
-const TopCharts = () => <div>TopCharts</div>;
+import React from 'react'
+import { selectAllPlayer } from '../redux/features/playerSlice';
+import { useSelector } from 'react-redux';
+import { useGetTopChartsQuery } from '../redux/services/shazamCoreApi';
+import { Error, Loader, SongCard } from '../components';
 
-export default TopCharts;
+const TopCharts = () => {
+   const { isPlaying, activeSong } = useSelector(selectAllPlayer);
+   const { data, isFetching, error } = useGetTopChartsQuery();
+
+   if (isFetching) return <Loader title={'Loading top songs'} />
+   if (error) return <Error />
+
+   return (
+      <React.Fragment>
+         <div className="px-6 mt-8 flex flex-col">
+            <h2 className='text-3xl font-bold text-white text-left'>Top Songs</h2>
+            <div className='mt-4 flex flex-wrap justify-center gap-3 sm:justify-start'>
+               {
+                  data?.map((item, index) => (
+                     item?.artists && <SongCard
+                        key={item.key}
+                        data={data}
+                        song={item}
+                        isPlaying={isPlaying}
+                        activeSong={activeSong}
+                        index={index}
+                        homePage={'Around You'}
+                     />
+                  ))
+               }
+            </div>
+         </div>
+      </React.Fragment>
+   )
+}
+
+export default TopCharts
