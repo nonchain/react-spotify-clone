a1c44f4173017521534c2ec9312c9a0e3612da4c
progali007 <code.progali@gmail.com>
2022-10-15 12:39:22 +0330

    Designed page

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -17,45 +17,44 @@ const ArtistDetails = () => {
   if (isFetching) return <Loader />
   if (error) return <Error />
 
+  const artistsSongs = Object.values(artistData?.songs);
+  const artistsData = artistData?.artists[artistId]?.attributes;
+  const artistArt = [...artistsData.artwork?.url].slice(0, -13).join('') + '512x512bb.jpg';
+
+  console.log(artistsData);
   return (
     <React.Fragment>
-      <div className='mt-8 flex flex-col'>
-        {/*====== HEADER =====* /}
-        {/* <div className='w-full h-80 relative overflow-hidden lg:h-96'>
-          <img src={songData?.images?.coverart} alt="" className='w-full h-full object-cover' />
-          <div className="absolute inset-0 bg-[#00000058] z-10"></div>
-          <div className='px-4 py-6 w-full h-full flex flex-col justify-between absolute inset-0 z-20 md:px-8 md:py-10'>
-            <div className="flex items-center justify-between">
-              <span className='text-xs text-gray-300 '>Discover / Song Details</span>
-              <img src={more} alt="more" />
-            </div>
-
+      <div className='flex flex-col'>
+        {/*====== HEADER =====*/}
+        <div className='px-4 w-full flex items-start justify-start gap-4'>
+          <img src={artistArt} alt="" className='w-28 h-28 rounded-lg overflow-hidden object-cover' />
+          <div className='flex flex-col justify-between'>
             <div className='flex items-end justify-between'>
-              <div className='flex flex-col'>
-                <h3 className='text-sm text-gray-100'>ARTIST</h3>
-                <Link to={song?.artists ? `/artist/${song?.artists[0]?.adamid}` : '/top-artists'}>
-                  <h1 className='text-5xl text-white'>{songData?.subtitle}</h1>
-                </Link>
-              </div>
-              <div className='p-3 w-12 bg-white/80 cursor-pointer flex items-center justify-center overflow-hidden rounded-full hover:bg-white duration-300'>
-                <img src={favorite} alt="favorite" className='' />
+              <div className='h-28 flex flex-col justify-between'>
+                <div className="flex flex-col">
+                <h1 className='text-3xl text-white'>{artistsData?.name}</h1>
+                <div className="flex items-center">
+                  {
+                    artistsData.genreNames?.map((genre, i) => <p className='text-white text-sm'>{genre}{i < artistsData.genreNames.length - 1 && ' - '}</p>)
+                  }
+                </div>
+                </div>
+                <button className='px-5 py-2 max-w-max rounded-full bg-[#1DB954] text-white text-sm cursor-pointer place-self-end'>Follow</button>
               </div>
             </div>
           </div>
-        </div> */}
+        </div>
         {/*====== BODY =====*/}
         <div className='px-6 pt-8 flex flex-col md:px-8 md:pt-10'>
-          {
-            console.log(Object.values(artistData?.songs))
-          }
+                  <h2 className='text-2xl text-white font-semibold'>Most listened</h2>
           <div className='mt-4 flex flex-wrap justify-center gap-3 sm:justify-start'>
             {
-              Object.values(artistData?.songs).map((item, index) => {
+              artistsSongs.map((item, index) => {
                 return <ArtistSongCard
                   key={`${item?.id}`}
                   song={item.attributes}
 
-                  data={Object.values(artistData?.songs)}
+                  data={artistsSongs}
                   index={index} />
               })
             }
